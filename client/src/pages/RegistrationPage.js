b6f79ad6b3908179e1ccb5d8f94e3822834690ad
root <root@LAPTOP-24INA7J4.localdomain>
2020-06-28 22:23:54 -0400

    fixed dependencies

--- client/src/pages/RegistrationPage.js
+++ client/src/pages/RegistrationPage.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Login from '../components/Registration';
+import Login from '../components/SignUp';
 import PageTitle from '../components/PageTitle';
 
 const RegistrationPage = () =>
