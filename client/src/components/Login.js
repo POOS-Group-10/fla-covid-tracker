04445ec907bbf4fa234ad86533679d8aa3530d7c
root <root@LAPTOP-24INA7J4.localdomain>
2020-07-03 10:43:46 -0400

    used href for routing

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,67 +1,59 @@
-import '../App.css';
-import React, {useState} from 'react';
-import { Link, useHistory } from 'react-router-dom';
-import axios from 'axios';
+import "../App.css";
+import React, { useState } from "react";
+import { Link, useHistory } from "react-router-dom";
+import axios from "axios";
 
-const Login = () =>
-{   
-    const history = useHistory();
+const Login = () => {
+  const [userName, setUserName] = useState("");
+  const [password, setPassword] = useState("");
 
-    const [userName, setUserName] = useState('');
-    const [password, setPassword] = useState('');
-    
-    const doLogin = async event => 
-    {    
-        event.preventDefault(); // Stops the browser from refreshing
-        
-        const payload = {
-            userName: userName,
-            password: password,
-        };
-        console.log('payload is ' + payload.userName + ' ' + payload.password);
-        axios({
-            url: '../api/Login', // React app is communicating with the server by this route
-            method: 'POST', // GET is used by default
-            data: payload
-        })
-            // These are promises
-            .then((response) => {
-              console.log('Data has been received ' + response.data);
-              history.push('/Home');
-              
-            })
-            .catch(() => {
-              console.log('Internal server error');
-            });
-           
+  const doLogin = async (event) => {
+    event.preventDefault(); // Stops the browser from refreshing
+
+    const payload = {
+      userName: userName,
+      password: password,
     };
+    console.log("payload is " + payload.userName + " " + payload.password);
+    axios({
+      url: "../api/Login", // React app is communicating with the server by this route
+      method: "POST", // GET is used by default
+      data: payload,
+    })
+      // These are promises
+      .then((response) => {
+        window.location.href = "/Home";
+      })
+      .catch(() => {
+        console.log("Internal server error");
+      });
+  };
 
-        return(
-            <div className="app">
-            <form onSubmit={doLogin}>
-                <div className = "form-input">
-                    <input
-                    type="text"
-                    placeholder="Username"
-                    name="username"
-                    onChange={e => setUserName(e.target.value)}
-                    />
-                </div>
-                <div className = "form-input">
-                    <input
-                    type="password"
-                    placeholder="Password"
-                    name="password"
-                    onChange={e => setPassword(e.target.value)}
-                    />
-                </div>
-                <button>Submit</button>
-            </form>
+  return (
+    <div className="app">
+      <form onSubmit={doLogin}>
+        <div className="form-input">
+          <input
+            type="text"
+            placeholder="Username"
+            name="username"
+            onChange={(e) => setUserName(e.target.value)}
+          />
+        </div>
+        <div className="form-input">
+          <input
+            type="password"
+            placeholder="Password"
+            name="password"
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+        <button>Submit</button>
+      </form>
 
-            <Link to="/SignUp">Don't have an account? Sign up.</Link>
-            </div>
-        );
-
-}
+      <Link to="/SignUp">Don't have an account? Sign up.</Link>
+    </div>
+  );
+};
 
 export default Login;
