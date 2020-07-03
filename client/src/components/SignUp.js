b6927bfaf40646bd46617a08ee02a80c58a55537
John <48269648+jstoner318@users.noreply.github.com>
2020-07-02 20:22:44 -0400

    Update SignUp.js

--- client/src/components/SignUp.js
+++ client/src/components/SignUp.js
@@ -1,7 +1,7 @@
 // John Stoner
 import '../App.css';
 import React, {useState} from 'react';
-import { Link } from 'react-router-dom'
+import { Link } from 'react-router-dom';
 import axios from 'axios';
 
 function SignUp() 
