03c66964178f59c7a9573df2e0398e8db1ce868f
tugitit <tule1102@gmail.com>
2020-07-03 22:12:48 -0400

    message

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -2,6 +2,8 @@ import "../App.css";
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import axios from "axios";
+import  { Redirect } from 'react-router-dom'
+
 
 const Login = () => {
 
