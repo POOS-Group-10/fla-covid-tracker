68a1a47015102758dc0dd427f0bddd7a2625e550
tugitit <tule1102@gmail.com>
2020-07-13 22:59:32 -0400

    done

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import fetch from 'node-fetch';
+import axios from "axios";
+
 
 import '../App.css';
 
