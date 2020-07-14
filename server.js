46ab0e42ba8707947ed2ec846cdb936bbba5cbd4
tugitit <tule1102@gmail.com>
2020-07-13 20:39:43 -0400

    login

--- server.js
+++ server.js
@@ -112,7 +112,7 @@ app.use(session({
   //   return uuid() // use UUIDs for session IDs
   // },
   secret: 'keyboard cat',
-  resave: false,
+  // resave: false,
   saveUninitialized: true,
   resave: true,  // connect-mongo update JR
   store: new MongoStore({ mongooseConnection: mongoose.connection }) // connect-mongo update JR
