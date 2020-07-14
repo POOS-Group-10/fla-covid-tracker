2a7dc33f8edc1aede6e575bf4357876b715eef3d
Jack0r0 <54642527+Jack0r0@users.noreply.github.com>
2020-07-14 11:54:22 -0400

    bugging

--- client/src/components/SignUp.js
+++ client/src/components/SignUp.js
@@ -43,8 +43,8 @@ const SignUp = () =>
             console.log("Entered the then from first Axios")
             console.log("response is " + response)
             const test_whats_inside = response      //Testing to see whats in resposne
-            console.dir(test_whats_inside)   //^^
-            console.log(response.data.taken);
+            console.dir('response.dir :' + test_whats_inside)   //^^
+            console.log('response token' + response.data.taken);
             if (response.data.taken === "1") {
                 setMessage(response.data.msg);
                 return;
