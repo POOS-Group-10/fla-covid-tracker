8a79637922adc26b4212c3158cb67a427587e551
Jack0r0 <54642527+Jack0r0@users.noreply.github.com>
2020-07-21 20:37:24 -0400

    Unit testin

--- test/test.js
+++ test/test.js
@@ -6,6 +6,7 @@ const { model } = require('../models/user.js');
 const nodemailer = require('nodemailer');
 var session = require('express-session');
 const bcrypt = require('bcrypt');
+const { holdReady } = require('jquery');
 
 // const { response } = require('express');
 
@@ -53,8 +54,6 @@ describe('User Registration',function(done) {
 
 // create a new post 
 describe('Create a new Post', function(done) {
-    
-
     user2 = {              
         title: 'unit testing suck',
         body: 'it really sucks',
@@ -75,13 +74,97 @@ describe('Create a new Post', function(done) {
             if (err) throw err; 
             expect(res.statusCode == 200);
         })
-        .end(function(err){
-            if(err) return done(err);
-        });
+        // .end(function(err){
+        //     if(err) return done(err);
+        // });
 
         done();
     })
 });
+
+// Logn In 
+describe('Login Api', function(done) {
+    user2 = {              
+        userName: "ho",
+        password: "ho"
+    };
+
+    const agent = request.agent(server);
+
+
+    it('Check Login', function(done){
+        
+        agent.post('/api/Login')
+
+        .send(user2)
+        .expect(200)
+        .expect(function(res, err) {
+            if (err) throw err; 
+            expect(res.statusCode == 200);
+        })
+        // .end(function(err){
+        //     if(err) return done(err);
+        // });
+
+        done();
+    })
+});
+
+// Get Post
+describe('Get Post form DB', function(done) {
+    user2 = {              
+        userName: "ho"
+    };
+
+    const agent = request.agent(server);
+
+
+    it('Get Post from DB', function(done){
+        
+        agent.post('/api/getPosts')
+
+        .send(user2)
+        .expect(200)
+        .expect(function(res, err) {
+            if (err) throw err; 
+            expect(res.statusCode == 200);
+        })
+        // .end(function(err){
+        //     if(err) return done(err);
+        // });
+
+        done();
+    })
+});
+
+// Find User
+describe('Find User form DB', function(done) {
+    user2 = {              
+        userName: "ho"
+    };
+
+    const agent = request.agent(server);
+
+
+    it('Find User from DB', function(done){
+        
+        agent.post('/api/findUser')
+
+        .send(user2)
+        // .expect(200)
+        .expect(function(res, err) {
+            if (err) throw err; 
+            expect(res.body.taken == '1')
+            expect(res.statusCode == 200);
+        })
+        // .end(function(err){
+        //     if(err) return done(err);
+        // });
+
+        done();
+    })
+});
+
 
 
 
